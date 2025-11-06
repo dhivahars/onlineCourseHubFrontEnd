765d9633638b68a9899f500be9e02978a0b62d9f
RahulR015 <rahulr15ec@gmail.com>
2025-11-06 18:16:22 +0530

    Created button component in shared folder

--- src/app/modules/auth/login/login.ts
+++ src/app/modules/auth/login/login.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { Button } from '../../../shared/button/button';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [Button],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
